028ea21bb676e721b2a4036a33c667e45f1e1ea8
Irawan Nurhas <irawan.nurhas@im-c.de>
2024-07-01 15:53:02 +0200

    init core

---
+++ app/src/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./config/swagger');
+const connectWithRetry = require('./config/database');
+const dataRoutes = require('./routes/dataRoutes');
+
+const app = express();
+const port = 3000;
+
+// Connect to MongoDB
+connectWithRetry();
+
+// Middleware to parse JSON
+app.use(bodyParser.json());
+
+// Swagger setup
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Use API routes
+app.use('/api', dataRoutes);
+
+// Home route
+app.get('/', (req, res) => {
+  res.send('Welcome to the JSON API');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
