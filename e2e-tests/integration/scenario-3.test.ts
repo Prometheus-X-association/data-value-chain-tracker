9c0e4f8225439f69d871cb90cc458a6c1277b5ae
mircea saracin <158305189+sesoman@users.noreply.github.com>
2025-07-23 13:05:24 +0300

    Update scenario-3.test.ts

--- e2e-tests/integration/scenario-3.test.ts
+++ e2e-tests/integration/scenario-3.test.ts
@@ -26,8 +26,6 @@ describe("Scenario 3: Educational Data Provider and AI Service Provider", functi
     // Setup provider and wallets
     provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
 
-    await provider.send("hardhat_reset", []);
-
     dataProvider = new ethers.Wallet(
       "0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a",
       provider
